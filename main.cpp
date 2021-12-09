4b928a1a70f067a7550222378123836ef763b813
and028 <91878390+and028@users.noreply.github.com>
2021-12-09 17:47:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
 
-int main(){
-  return 0;
+    char a[10][20];
+    char b[20];
+
+    cin>>a[0];
+    cin>>a[1];
+    cin>>a[2];
+    cin>>a[3];
+    cin>>a[4];
+    cin>>a[5];
+    cin>>a[6];
+    cin>>a[7];
+    cin>>a[8];
+    cin>>a[9];
+    cin>>b;
+
+    int p=posizione(a,b);
+    if(p){
+        cout<<p-1;
+    }else{
+        cout<<"non presente";
+    }
+
+    return 0;
 }
